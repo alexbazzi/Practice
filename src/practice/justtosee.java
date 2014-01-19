fd1dc2a5d27c1e7f4e4c0bed814d9f907154da14
alexbazzi <alexbazzi@localhost>
2014-01-19 12:44:26 -0800

    FInished JPanel project

---
+++ src/practice/justtosee.java
@@ -0,0 +1,17 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+
+package practice;
+
+/**
+ *
+ * @author alexbazzi
+ */
+public class justtosee {
+    public static void main(String[] args)
+    {
+        System.out.println("Imma fagit");
+    }
+}
