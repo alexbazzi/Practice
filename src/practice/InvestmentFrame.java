9a1168ff4e188074339ed424bf32c271b925c920
alexbazzi <alexbazzi@localhost>
2014-01-19 12:30:10 -0800

    FInished JPanel project

--- src/practice/InvestmentFrame.java
+++ src/practice/InvestmentFrame.java
@@ -31,4 +31,54 @@ public class InvestmentFrame extends JFrame
         this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         
     }
+    
+    public void createTextField()
+    {
+        rateLabel = new JLabel("Interest rate: ");
+        
+        final int FIELD_WIDTH = 10;
+        rateField = new JTextField(FIELD_WIDTH);
+        rateField.setText("" + DEFAULT_RATE);
+        
+    }
+    
+    public void createButton()
+    {
+        button = new JButton("Add interest");
+        
+        class AddInterestListener implements ActionListener
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                double rate = Double.parseDouble(rateField.getText());
+                double interest = account.getBalance()*(rate/100);
+                account.deposit(interest);
+            }
+        }
+        ActionListener listener = new AddInterestListener();
+        button.addActionListener(listener);
+    }
+    
+    public void createPanel()
+    {
+        JPanel panel = new JPanel();
+        panel.add(rateLabel);
+        panel.add(rateField);
+        panel.add(button);
+        panel.add(resultLabel);
+        this.add(panel);
+    }
+    
+    private JLabel rateLabel;
+    private JTextField rateField;
+    private JButton button;
+    private JLabel resultLabel;
+    private JPanel panel;
+    private BankAccount account;
+    
+    private static final int FRAME_WIDTH = 500;
+    private static final int FRAME_HEIGHT = 200;
+    
+    private static final double DEFAULT_RATE = 5;
+    private static final double INITIAL_BALANCE = 1000;
 }
---
+++ src/practice/InvestmentViewer3.java
@@ -0,0 +1,20 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+
+package practice;
+import javax.swing.JFrame;
+/**
+ *
+ * @author alexbazzi
+ */
+public class InvestmentViewer3 
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
