4ae77a4a503fd7eda097afaf48bd1c7ebcc6e8f9
alexbazzi <Alex@192.168.0.69>
2014-01-19 10:48:55 -0500

    Just finished a user interface that returns interest rate at the click of a button.

---
+++ src/practice/InvestmentViewer2.java
@@ -0,0 +1,51 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+
+package practice;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+/**
+ *
+ * @author Alex
+ */
+public class InvestmentViewer2 extends InvestmentViewer
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Add interest");
+        
+        final BankAccount account = new BankAccount(INITIAL_BALANCE);
+        
+        final JLabel label = new JLabel("balance: " + account.getBalance());
+        
+        JPanel panel = new JPanel();
+        panel.add(button);
+        panel.add(label);
+        frame.add(panel);
+        
+        class AddInterestListener implements ActionListener
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                account.addInterest();
+                label.setText("balance: " + account.getBalance());
+            }
+        }
+        
+        ActionListener listener = new AddInterestListener();
+        button.addActionListener(listener);
+        
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
+}
